debcaa3373f314592419cd1b04bcdce2d74ba3d1
Mderavi <deravi.mahsa@gmail.com>
2025-05-28 14:04:28 +1000

    adding the loading and error status

--- src/App.js
+++ src/App.js
@@ -1,14 +1,47 @@
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
 
+const initialState = {
+  questions: [],
+  //loading, error, ready, active, finished
+  status: "Loading",
+};
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataRecevied":
+      return {
+        ...state,
+        questions: action.payload,
+        statuse: "ready",
+      };
+    case "dataFailed":
+      return {
+        ...state,
+        statuse: "error",
+      };
+
+    default:
+      throw new Error("Action unknown");
+  }
+}
 export default function App() {
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+  useEffect(function () {
+    fetch("http://localhost:9000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataRecevied", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
   return (
     <div className="app">
       <Header />
 
       <Main>
-        <p>1/15</p>
-        <p>Questions?</p>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
       </Main>
     </div>
   );
